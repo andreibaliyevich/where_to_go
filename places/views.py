9c137176789ebf75d254df1c6e99e7b981e25527
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-09-08 19:21:52 +0300

    Update place_detail view

--- places/views.py
+++ places/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Place
 
@@ -32,4 +32,20 @@ def index(request):
 def place_detail(request, pk):
     """ Place detail """
     place = get_object_or_404(Place, pk=pk)
-    return HttpResponse(place.title)
+
+    imgs = []
+    for img in place.image_set.all():
+        imgs.append(img.image.url)
+
+    data = {
+        'title': place.title,
+        'imgs': imgs,
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.coordinates_lng,
+            'lat': place.coordinates_lat
+        }
+    }
+    return JsonResponse(data,
+        safe=False, json_dumps_params={'ensure_ascii': False})
