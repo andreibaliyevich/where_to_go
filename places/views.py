dd92cbcf742bbe9e9e26e300a06d4a18dac8115c
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-09-08 07:14:57 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -3,4 +3,36 @@ from django.shortcuts import render
 
 def index(request):
     """ Home page """
-    return render(request, 'index.html')
+    places_json = {
+      "type": "FeatureCollection",
+      "features": [
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.62, 55.793676]
+          },
+          "properties": {
+            "title": "«Легенды Москвы",
+            "placeId": "moscow_legends",
+            "detailsUrl": "/static/places/moscow_legends.json"
+          }
+        },
+        {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [37.64, 55.753676]
+          },
+          "properties": {
+            "title": "Крыши24.рф",
+            "placeId": "roofs24",
+            "detailsUrl": "/static/places/roofs24.json"
+          }
+        }
+      ]
+    }
+    context = {
+        'places': places_json,
+    }
+    return render(request, 'index.html', context)
