c1d81dc9f8193a8d89c8ebfb6f1bc7f4b063162e
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-09-08 19:48:53 +0300

    Update admin.py

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Place, Image
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+    fields = ('image', 'get_preview', 'position')
+    readonly_fields = ('get_preview',)
+
+    def get_preview(self, obj):
+        if obj.image.height < 200:
+            img_height = obj.image.height
+        else:
+            img_height = 200
+
+        return mark_safe('<img src="{url}" height={height} />'.format(
+            url=obj.image.url,
+            height=img_height,
+        )
+    )
 
 
 class PlaceAdmin(admin.ModelAdmin):
