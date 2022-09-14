83809b06f9f49662661772259e1112e8aeb87ca8
Andrei Baliyevich <andreibaliyevich@gmail.com>
2022-09-14 12:26:43 +0300

    Update Views

--- places/utilities.py
+++ places/utilities.py
@@ -1,9 +1,24 @@
 from os.path import splitext
+from django.urls import reverse
 from django.utils import timezone
 
 
 def get_image_path(instance, filename):
-    """ Get path of image """
     filepath = timezone.now().strftime('%Y/%m/%d/%H%M%S%f')
     file_ext = splitext(filename)[1].lower()
     return f'images/{ filepath }{ file_ext }'
+
+
+def get_feature_dict(place):
+    return {
+        'type': 'Feature',
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [place.longitude, place.latitude]
+        },
+        'properties': {
+            'title': place.title,
+            'placeId': place.id,
+            'detailsUrl': reverse('places:place_detail', args=[place.id])
+        }
+    }
--- places/views.py
+++ places/views.py
@@ -1,48 +1,31 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Place
+from .utilities import get_feature_dict
 
 
 def index(request):
-    features = []
-    for place in Place.objects.all():
-        features.append({
-            'type': 'Feature',
-            'geometry': {
-                'type': 'Point',
-                'coordinates': [place.longitude, place.latitude]
-            },
-            'properties': {
-                'title': place.title,
-                'placeId': place.id,
-                'detailsUrl': f'/places/{ place.id }/'
-            }
-        })
-    places_json = {
-      "type": "FeatureCollection",
-      "features": features
-    }
+    features = [get_feature_dict(place) for place in Place.objects.all()]
     context = {
-        'places': places_json,
+        'places': {
+          "type": "FeatureCollection",
+          "features": features,
+        },
     }
     return render(request, 'index.html', context)
 
 
 def place_detail(request, pk):
     place = get_object_or_404(Place, pk=pk)
-
-    imgs = []
-    for img in place.images.all():
-        imgs.append(img.image.url)
-
-    data = {
+    imgs = [img.image.url for img in place.images.all()]
+    raw_place = {
         'title': place.title,
         'imgs': imgs,
         'description_short': place.description_short,
         'description_long': place.description_long,
         'coordinates': {
             'lng': place.longitude,
-            'lat': place.latitude
+            'lat': place.latitude,
         }
     }
-    return JsonResponse(data, json_dumps_params={'ensure_ascii': False})
+    return JsonResponse(raw_place, json_dumps_params={'ensure_ascii': False})
